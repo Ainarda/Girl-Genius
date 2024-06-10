8e1e3720429cd2acaf14c564bc1c3c33dde2f340
pikafun <antoshka16.08.1999@gmail.com>
2024-06-10 21:53:19 +0300

    Changing unlockingobject

--- Assets/Scripts/Maison/UnlockingEnvironment.cs
+++ Assets/Scripts/Maison/UnlockingEnvironment.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,7 +13,7 @@ public class UnlockingEnvironment : MonoBehaviour
     [SerializeField]
     private int cost;
     [SerializeField]
-    private Text costDisplay;
+    private TextMeshProUGUI costDisplay;
     [SerializeField]
     private Button buyButton;
     [SerializeField]
