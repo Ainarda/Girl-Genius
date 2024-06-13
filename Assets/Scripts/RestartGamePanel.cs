bf5bf5118aee6ff46ac44b5bfa5a416c3c879f09
DasDaOne <mr.ars355@GMAIL.COM>
2024-06-13 15:19:22 +0400

    Changed Level101 end panel

--- Assets/Scripts/RestartGamePanel.cs
+++ Assets/Scripts/RestartGamePanel.cs
@@ -7,38 +7,20 @@ using UnityEngine.UI;
 public class RestartGamePanel : MonoBehaviour
 {
     [SerializeField] private Button restartGameButton;
-    [SerializeField] private Button[] gameButtons;
-    [SerializeField] private string[] gameLinks;
-    
+
     private void OnEnable()
     {
         restartGameButton.onClick.AddListener(RestartGame);
-
-        for (int i = 0; i < gameButtons.Length; i++)
-        {
-            int index = i;
-            gameButtons[i].onClick.AddListener(() => OpenGame(index));
-        }
     }
 
     private void OnDisable()
     {
         restartGameButton.onClick.RemoveListener(RestartGame);
-
-        foreach (Button button in gameButtons)
-        {
-            button.onClick.RemoveAllListeners();
-        }
     }
 
     private void RestartGame()
     {
         PlayerData.CurrentLvl = 1;
         PlayerData.LoadNextLevel();
-    }
-
-    private void OpenGame(int buttonIndex)
-    {
-        Application.OpenURL(gameLinks[buttonIndex]);
     }
 }
